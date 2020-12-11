ca7a32eb8ac2c6614f9aff68de2e43c6aa80cd91
abpwrs <alexander-powers@uiowa.edu>
2020-12-10 23:51:34 -0600

    test: heroku deploy

--- app/app.py
+++ app/app.py
@@ -18,3 +18,8 @@ assets.register('js_all', js)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
